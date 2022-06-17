5dc327d772bcfe469cece9f08879aeb55ca5fe93
jimmy lelievre <jimmylelievre@iMac-de-jimmy.local>
2022-06-17 21:01:06 +0200

    Setting up search bar and table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,35 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const Table = () => {
   return (
     <div className="table">
+      <SearchBar />
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>Date of birth</th>
+            <th>Adress</th>
+            <th>Start Day</th>
+            <th>Department</th>
           </tr>
         </thead>
+        <div className="line"></div>
         <tbody>
           <tr>
             <td>Jimmy Lelievre</td>
             <td>24 mars 2001</td>
+            <td>244, jaffray street</td>
+            <td>January 13, 2003</td>
+            <td>Sales</td>
+          </tr>
+          <tr>
+            <td>Jimmy Lelievre</td>
+            <td>24 mars 2001</td>
+            <td>244, jaffray street</td>
+            <td>January 13, 2003</td>
+            <td>Sales</td>
           </tr>
         </tbody>
       </table>
